9c3166914c66c7298420e18cc77ceeda9633feec
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-10-03 11:54:07 +0530

    Updated

--- src/components/VideoUpload.jsx
+++ src/components/VideoUpload.jsx
@@ -8,7 +8,6 @@ const VideoUpload = () => {
         <h1 className='dark:text-white'>Upload vidoes</h1>
       <form class="flex items-center space-x-9">
         <div class="shrink-0">
-          {/* <img class="h-16 w-16 object-cover rounded-full" src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1361&q=80" alt="Current profile photo" /> */}
           <CiYoutube className='text-2xl' />
         </div>
         <label class="block">
